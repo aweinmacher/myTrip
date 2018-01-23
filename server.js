4f9dc08427768a2b1b4f1a962a2650c37abf3f05
Anna W <aweinmacher@gmail.com>
2018-01-23 12:10:44 +0200

    creates the database at localhost

--- server.js
+++ server.js
@@ -14,45 +14,45 @@ app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var Post = require('./models/models.post');
-var Trip = require('./models/models.trip');
-var Todo = require('./models/models.todo');
+var User = require('./models/models').user;
+var Trip = require('./models/models').trip;
+var Todo = require('./models/models').todo;
 
 // RUN ONLY ONCE ---- CREATION CODE !!! CREATION CODE !!! CREATION CODE !!! CREATION CODE
-var user1 = new User({
-  name: 'User 1',
-  trips: []
-});
-var trip1 = new Trip({
-  user: user1._id,
-  country: 'Country 1',
-  flight: {
-      number: 'Team-106',
-      from: 'Elevation',
-      to: 'First IT job',
-      departure: {
-          date: '23-Jan-2018',
-          time: '9:00'
-      },
-      arrival: {
-          date: '25-Jan-2018',
-          time: '12:00'
-      }
-  },
-  todos: []
-});
-var todo = new Todo({
-  user: user1._id,
-  text: 'Enjoy awesome stuff we learn'
-});
+// var user1 = new User({
+//   name: 'User 1',
+//   trips: []
+// });
+// var trip1 = new Trip({
+//   user: user1._id,
+//   country: 'Country 1',
+//   flight: {
+//       number: 'Team-106',
+//       from: 'Elevation',
+//       to: 'First IT job',
+//       departure: {
+//           date: '23-Jan-2018',
+//           time: '9:00'
+//       },
+//       arrival: {
+//           date: '25-Jan-2018',
+//           time: '12:00'
+//       }
+//   },
+//   todos: []
+// });
+// var todo = new Todo({
+//   user: user1._id,
+//   text: 'Enjoy awesome stuff we learn'
+// });
 
-todo.save(); // user1._id already exists, so it should be pushed automatically -> we can save todo
+// todo.save(); // user1._id already exists, so it should be pushed automatically -> we can save todo
 
-trip1.todos.push(todo); // in trip1 user1._id comes automatically, so before saving we need to push only a todo
-trip1.save(); // save trip before pushing it into user
+// trip1.todos.push(todo); // in trip1 user1._id comes automatically, so before saving we need to push only a todo
+// trip1.save(); // save trip before pushing it into user
 
-user1.trips.push(trip1);
-user1.save();
+// user1.trips.push(trip1);
+// user1.save();
 // END OF CREATION CODE !!! END OF CREATION CODE !!! END OF CREATION CODE !!! END OF CREATION CODE !!!
 
 
