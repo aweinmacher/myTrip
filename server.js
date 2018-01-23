1c5bfd95544642a2951ad9cbb3dcdeef5cec0c8b
Anna W <aweinmacher@gmail.com>
2018-01-23 14:30:38 +0200

    dummy get function

--- server.js
+++ server.js
@@ -18,9 +18,66 @@ var User = require('./models/models').user;
 var Trip = require('./models/models').trip;
 var Todo = require('./models/models').todo;
 
+// 1) to check if the user exists by email and send data back if exists
+app.get('/users', function (req, res) {
+  // returns an object if user exists
+  res.send({
+      "_id" : "5a6708ced9d33a19c8584bcf",
+      "email": "user@gmail.com",
+      "trips" : [
+        {
+          "_id" : "5a6708ced9d33a19c8584bd0",
+          "flight" : {
+              "departure" : {
+                  "date" : "23-Jan-2018",
+                  "time" : "9:00"
+              },
+              "arrival" : {
+                  "date" : "25-Jan-2018",
+                  "time" : "12:00"
+              },
+              "number" : "Team-106",
+              "from" : "Elevation",
+              "to" : "First IT job"
+          },
+          "todos" : [{
+              "_id" : "5a6708ced9d33a19c8584bd1",
+              "user" : "5a6708ced9d33a19c8584bcf",
+              "text" : "Enjoy awesome stuff we learn"
+          }],
+          "user" : ObjectId("5a6708ced9d33a19c8584bcf"),
+          "country" : "Country 1"
+      }
+      ],
+      "name" : "User 1"
+  })
+})
+
+
+// 2) to handle adding a new user
+app.post('/users', function (req, res) {
+  // returns an object with user
+})
+
+// 3) to handle creating a Trip with a given country and pushing it into user.trips
+
+// 4) to handle adding a todo
+
+// 5) to handle getting all todos for a certain country
+
+
+
+
+
+
+
+
+
+
 // RUN ONLY ONCE ---- CREATION CODE !!! CREATION CODE !!! CREATION CODE !!! CREATION CODE
 // var user1 = new User({
 //   name: 'User 1',
+//   email: 'user@gmail.com',
 //   trips: []
 // });
 // var trip1 = new Trip({
